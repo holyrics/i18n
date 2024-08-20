d18cdd6d843d38c0d7837618fdbc31991ba2a55f
Holyrics <43863376+holyrics@users.noreply.github.com>
2024-08-20 12:27:58 -0300

    Create v2-23-0-details.php

---
+++ site/changelog/en/v2-23-0-details.php
@@ -0,0 +1,10 @@
+<b>Modules</b>
+Allows the creation of solutions using JavaScript that interact with various program resources, allowing you to change and/or adapt the program's operation, create buttons that execute customized actions, real-time triggers, among other possibilities, all in one place
+Solutions can be easily shared through a single JavaScript code (or saved as a file) and also made available in the program for other users to add ready-made solutions, through the online repository
+
+<b>JSLib</b>
+- Compatibility with websocket connections via simple string (instead of json) and the possibility of creating tcp connections that keep the connection active.
+This allows the implementation of other specific protocols and communication with other programs, such as QLC, MTQQ, Soundcraft, etc.
+
+- Arrow Function
+Although the system uses ECMA 5.1 as a base, a real-time parser was implemented to allow the use of Arrow Functions in the JavaScript codes used in the program.
