fd036acd71dd08befffdac0fae27ec071a1b5af9
Holyrics <43863376+holyrics@users.noreply.github.com>
2024-10-16 19:20:38 -0300

    changelog v2.23.1

---
+++ site/changelog/en/v2-23-1.php
@@ -0,0 +1,4 @@
+<dl>
+  <dt>Fixed issues</dt>
+    <dd>• Fixed possible high CPU usage and crash when there are many files in the program's media tabs;</dd>
+</dl>
---
+++ site/changelog/pt/v2-23-1.php
@@ -0,0 +1,4 @@
+<dl>
+  <dt>Correções</dt>
+    <dd>• Corrigido possível uso elevado de CPU e travamento quando há muitos arquivos nas abas de mídia do programa;</dd>
+</dl>
